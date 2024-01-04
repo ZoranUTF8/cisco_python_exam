8b075e3fdc921bb49793c060da68eee536d8b925
ZoranUTF8 <janjiczoran23@gmail.com>
2024-01-04 23:53:39 +0900

    XOMPLETE 3.4.6  LAB

--- 3.4 Section 4 – Lists/3.4.11   LAB   The basics of lists ‒ the Beatles/main.py
+++ 3.4 Section 4 – Lists/3.4.11   LAB   The basics of lists ‒ the Beatles/main.py
@@ -8,8 +8,24 @@
 # Write a program that reflects these changes and lets you practice with the concept of lists. Your task is to:
 #
 # step 1: create an empty list named beatles;
+beatles = []
 # step 2: use the append() method to add the following members of the band to the list: John Lennon, Paul McCartney, and George Harrison;
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
 # step 3: use the for loop and the append() method to prompt the user to add the following members of the band to the list: Stu Sutcliffe, and Pete Best;
-# step 4: use the del instruction to remove Stu Sutcliffe and Pete Best from the list;
+for a in ["Stu Sutcliffe", "Pete Best"]:
+    beatles.append(a)
+print(beatles)
+# step 4: use the del instruction to remove "Stu Sutcliffe" and Pete Best from the list;
+indexOf1 = beatles.index("Stu Sutcliffe")
+
+del beatles[indexOf1]
+
+indexOf2 = beatles.index("Pete Best")
+del beatles[indexOf2]
+print(beatles)
 # step 5: use the insert() method to add Ringo Starr to the beginning of the list.
+beatles.insert(0, "Ringo Starr")
+print(beatles)
 # By the way, are you a Beatles fan? (The Beatles is one of Greg's favorite bands. But wait...who's Greg...?)
