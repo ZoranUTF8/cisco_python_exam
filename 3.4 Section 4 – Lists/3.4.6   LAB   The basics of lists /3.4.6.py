2a997f9bdc16738547d336051a656f99411a0d99
ZoranUTF8 <janjiczoran23@gmail.com>
2024-01-04 23:19:54 +0900

    ADD 3.4.6  LAB

--- 3.4 Section 4 – Lists/3.4.6   LAB   The basics of lists /3.4.6.py
+++ 3.4 Section 4 – Lists/3.4.6   LAB   The basics of lists /3.4.6.py
@@ -3,14 +3,16 @@
 #
 # Your task is to:
 #
-# write a line of code that prompts the user to replace the middle number in the list with an integer number entered by the user (Step 1)
-# write a line of code that removes the last element from the list (Step 2)
-# write a line of code that prints the length of the existing list (Step 3).
 
 hat = [1, 2, 3, 4, 5]
 mid = round(len(hat) / 2)
-inp1 = int(input("Replace the middle number with ?"))
 
+# write a line of code that prompts the user to replace the middle number in the list with an integer number entered by the user (Step 1)
+inp1 = int(input("Replace the middle number with ?"))
 hat[mid] = inp1
 
-print(hat)
+# write a line of code that removes the last element from the list (Step 2)
+del hat[len(hat)-1]
+
+# write a line of code that prints the length of the existing list (Step 3).
+print(len(hat))
--- 3.4 Section 4 – Lists/lists.py
+++ 3.4 Section 4 – Lists/lists.py
@@ -1,11 +1,26 @@
 # TODO numbers = [10, 5, 7, 2, 1]   may have different types. always numbered starting from zero.
-#  TODO list is a collection of elements, but each element is a scalar.
-numbers = [10, 5, 7, 2, 1]
-numbers[1] = numbers[2]
-numbers[0] = 100
-print(numbers)
-print(len(numbers))
+# TODO list is a collection of elements, but each element is a scalar.
+# TODO  The list is a type of data in Python used to store multiple objects
+# TODO It is an ordered and mutable collection of comma-separated items between square brackets, The order of elements is maintained, meaning they're stored and accessed in the same sequence you put them in.
+#
+# my_list = [1, None, True, 'I am a string', 256, 0]
+# print(my_list[3])  # outputs: I am a string
+# print(my_list[-1])  # outputs: 0
+#
+# my_list[1] = '?'
+# print(my_list)  # outputs: [1, '?', True, 'I am a string', 256, 0]
+#
+# my_list.insert(0, "first")
+# my_list.append("last")
+# print(my_list)  # outputs: ['first', 1, '?', True, 'I am a string', 256, 0, 'last']
+#
+# my_list_2 = [1, 'a', ["list", 64, [0, 1], False]]
+# print(my_list_2[2])
+#
+# my_list_3 = [1, 2, 3, 4]
+# del my_list_3[2]
+# print(my_list_3)  # outputs: [1, 2, 4]
+#
+# del my_list_3  # deletes the whole list
 
 
-del numbers[0]
-print(numbers)
